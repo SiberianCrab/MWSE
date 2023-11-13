cd5a3b8dd0a6967db0ab7dfffe90a9f3a36c90e6
SiberianCrab <46442176+SiberianCrab@users.noreply.github.com>
2023-11-13 23:34:38 +0300

    Update Settings.h

--- CSSE/Settings.h
+++ CSSE/Settings.h
@@ -169,38 +169,6 @@ namespace se::cs {
 			toml::value into_toml() const;
 		} landscape_window;
 
-
-		// Leveled Item window
-
-		struct LeveledItemWindowSettings {
-			int x_position = 0;
-			int y_position = 71;
-
-			WindowSize size = { 524, 230 };
-
-			ColumnSettings column_PC_Level = { 70u };
-			ColumnSettings column_Item_Name = { 170u };
-
-			void from_toml(const toml::value& v);
-			toml::value into_toml() const;
-		} leveled_item_window;
-
-		// Leveled Creature window
-
-		struct LeveledCreatureWindowSettings {
-			int x_position = 0;
-			int y_position = 71;
-
-			WindowSize size = { 524, 230 };
-
-			ColumnSettings column_PC_Level = { 70u };
-			ColumnSettings column_Creature_Name = { 170u };
-
-			void from_toml(const toml::value& v);
-			toml::value into_toml() const;
-		} leveled_creature_window;
-
-
 		struct ColorTheme {
 			std::array<unsigned char, 3> highlight_deleted_object_color = { 255, 235, 235 };
 			std::array<unsigned char, 3> highlight_modified_from_master_color = { 235, 255, 235 };
