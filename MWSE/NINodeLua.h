a4bb41db3e56d2709cd90056f9330d72cd289fa0
C3pa <41503714+C3pa@users.noreply.github.com>
2026-02-02 22:46:22 +0100

    Add niNode:traverse

--- MWSE/NINodeLua.h
+++ MWSE/NINodeLua.h
@@ -1,9 +1,45 @@
 #pragma once
 
 #include "NIObjectLua.h"
+#include "NIDefines.h"
 #include "NIDynamicEffect.h"
 
 namespace mwse::lua {
+	auto traverse(const NI::Node* node) {
+		std::queue<NI::Pointer<NI::AVObject>> queue;
+		std::function<void(const NI::AVObject*)> traverseChild = [&](const NI::AVObject* object) {
+			if (!object->isInstanceOfType(NI::RTTIStaticPtr::NiNode)) {
+				return;
+			}
+
+			const auto asNode = static_cast<const NI::Node*>(object);
+			for (auto& nodeChild : asNode->children) {
+				if (!nodeChild) {
+					continue;
+				}
+				queue.push(nodeChild);
+				traverseChild(nodeChild);
+			}
+		};
+
+		for (auto& child : node->children) {
+			if (!child) {
+				continue;
+			}
+			queue.push(child);
+			traverseChild(child);
+		}
+
+		return [&]() -> NI::Pointer<NI::AVObject> {
+			if (queue.empty()) {
+				return nullptr;
+			}
+			auto ret = queue.front();
+			queue.pop();
+			return ret;
+		};
+	}
+
 	template <typename T>
 	void setUserdataForNINode(sol::usertype<T>& usertypeDefinition) {
 		setUserdataForNIAVObject(usertypeDefinition);
@@ -22,6 +58,7 @@ namespace mwse::lua {
 		usertypeDefinition["detachChildAt"] = &NI::Node::detachChildAt_lua;
 		usertypeDefinition["detachEffect"] = &NI::Node::detachEffect;
 		usertypeDefinition["getEffect"] = &NI::Node::getEffect;
+		usertypeDefinition["traverse"] = traverse;
 	}
 
 	void bindNINode();
